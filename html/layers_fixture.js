374c403c46f3b22b531eed5df021fd56b398e7a8
Matthew Perry <mperry@ecotrust.org>
2013-04-21 15:24:02 -0700

    static viewer

---
+++ html/layers_fixture.js
@@ -0,0 +1,19 @@
+app.fixture = {
+    "layers": [
+        {
+            "name": "test_countries", "url": ["/tiles/test_countries/${z}/${x}/${y}.png"],
+            "opacity": 0.8, "lookups": {"field": null, "details": []}, "graphic": null, 
+            "default_on": true, "data_source": "", "subLayers": [],
+            "utfurl": null, "description": null, "arcgis_layers": null, "legend": null, "legend_title": null, 
+            "attributes": {"attributes": [], "compress_attributes": false, "event": "click", "title": null}, 
+            "fill_opacity": null, "learn_link": null, "type": "XYZ", "id": 1, "color": null, 
+            "legend_subtitle": null
+        }
+    ],
+    "themes": [
+        {
+            "layers": [1], 
+            "description": null, "display_name": "All Layers", "id": 1, "learn_link": "http://localhost:8088"
+        }
+   ]
+}
